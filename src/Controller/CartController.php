5159bcab2d03fdc2a41f63710a079fceb88d908c
kalvin <kalvin.daniel56300@gmail.com>
2022-01-22 13:05:25 +0100

    cart update

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,26 +2,39 @@
 
 namespace App\Controller;
 
-use App\Repository\ProductRepository;
 use Exception;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
 
 class CartController extends AbstractController
 {
     /**
      * @Route("/cart", name="cart")
      */
-    public function index(SessionInterface $session): Response
+    public function index(SessionInterface $session, ProductRepository $productRepository): Response
     {
 
         $cart = $session->get('panier', []);
-        print_r($cart);
+        $products = [];
+        $total = 0;
+        foreach ($cart as $key => $value) {
+            $product = $productRepository->find($key);
+            $products[] = [
+                "id" => $product->getId(),
+                "name" => $product->getName(),
+                "price" => $product->getPrice(),
+                "quantite" => $value
+            ];
+            $total += $product->getPrice();
+        }
         return $this->render('cart/index.html.twig', [
-            'cart' => $cart,
+            'products' => $products,
+            'total' => $total,
         ]);
     }
 
@@ -55,6 +68,40 @@ class CartController extends AbstractController
                 'status' => 400,
                 'message' => $e->getMessage()
             ], 400);
+        }
+        
+    }
+
+    /**
+     * @Route("/cart/delete/{id}", name="cart.delete")
+     */
+    public function delete(SessionInterface $session, Request $request, ProductRepository $productRepository): Response
+    {
+
+        $id = $request->attributes->get('id');
+        $product = $productRepository->find($id);
+        if (!$product)
+        {
+            throw $this->createNotFoundException('The product does not exist');
+        }
+        $csrfToken = $_GET['token'];
+        if ($this->isCsrfTokenValid('delete-item', $csrfToken))
+        {
+            $cart = $session->get('panier', []);
+            if(!isset($cart[$id])){
+                return $this->json([
+                    'status' => 404,
+                    'message' => 'nok'
+                ], 404);
+            }
+            unset($cart[$id]);
+            $session->set('panier', $cart);
+
+            $this->addFlash('alert', "Le produit {$product->getName()} a bien été supprimé !");
+            
+            return $this->redirectToRoute('cart');
+        }else{
+            throw new InvalidCsrfTokenException();
         }
         
     }
